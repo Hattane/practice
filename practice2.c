fe7bd00433a0e2c28ab810a1bb193fa280c36f01
Hattane <hattanefaiz.1@gmail.com>
2025-04-09 10:41:55 +0900

    Create practice2.c

---
+++ practice2.c
@@ -0,0 +1,12 @@
+ #include < stdio.h >
+ int main( void )
+ {
+ int x=10, y=10;
+ printf("x=%d\n" , x);
+ printf("++x value =%d\n", ++x);
+ printf("x=%d\n\n" , x);
+ printf("y=%d\n", y);
+ printf("value of y++ =%d\n", y++);
+ printf("y=%d\n", y);
+return 0;
+}
